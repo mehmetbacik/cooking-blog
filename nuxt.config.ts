ca03518249d7ef5dc3e2a0e8e2bea071f758e9c8
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-11-19 22:46:31 +0300

    chore: clean up nuxt.config.ts for a simpler and optimized setup
    - Removed unused configurations and redundant settings from nuxt.config.ts.
    - Simplified module declarations and ensured consistent formatting.
    - Improved clarity and maintainability of the configuration file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 import { defineNuxtConfig } from "nuxt/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  vite: {
-    plugins: [tsconfigPaths()],
-  },
 });
