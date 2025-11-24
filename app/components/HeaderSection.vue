efd28234dc043d651db73e4c685a753ec551d552
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-11-24 23:27:55 +0300

    feat: improve HeaderSection.vue structure and functionality
    - Updated HeaderSection.vue with improved layout and styling.
    - Integrated navigation and social data dynamically.
    - Enhanced component structure for better readability and scalability.
    - Applied SCSS improvements for spacing, typography, and responsive behavior.

--- app/components/HeaderSection.vue
+++ app/components/HeaderSection.vue
@@ -1,35 +1,48 @@
 <script setup lang="ts">
 import { navItems } from "../data/navData";
 import { socialItems } from "../data/socialData";
+const LogoPath = "/images/logo.svg";
 </script>
 
 <template>
   <nav class="navbar">
     <div class="container navbar__content">
-      <NuxtLink to="/" class="navbar__logo">
-        <span>Cooking Blog</span>
-      </NuxtLink>
-      <div class="navbar__links">
-        <ul>
-          <li v-for="item in navItems" :key="item.id">
-            <NuxtLink :to="item.link" class="nav-item" active-class="active">
-              {{ item.label }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <div class="navbar__socials">
-        <div class="social-links">
-          <a
-            v-for="social in socialItems"
-            :key="social.id"
-            :href="social.link"
-            target="_blank"
-            :aria-label="social.name"
-            class="social-item"
-          >
-            <img :src="social.iconUrl" :alt="social.name" />
-          </a>
+      <div class="row align-items-center">
+        <div class="col-12 col-lg-3 logo__wrapper">
+          <NuxtLink to="/" class="navbar__logo">
+            <img :src="LogoPath" alt="Logo" />
+          </NuxtLink>
+        </div>
+        <div class="col-12 col-lg-6 nav__wrapper">
+          <div class="navbar__links">
+            <ul>
+              <li v-for="item in navItems" :key="item.id">
+                <NuxtLink
+                  :to="item.link"
+                  class="nav-item"
+                  active-class="active"
+                >
+                  {{ item.label }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 social__wrapper">
+          <div class="navbar__socials">
+            <div class="social__links">
+              <a
+                v-for="social in socialItems"
+                :key="social.id"
+                :href="social.link"
+                target="_blank"
+                :aria-label="social.name"
+                class="social-item"
+              >
+                <img :src="social.iconUrl" :alt="social.name" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
