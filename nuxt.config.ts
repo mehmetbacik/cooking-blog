15e2adae2a5315506674984faa6872f43877edc1
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-11-24 23:02:01 +0300

    feat: update nuxt.config with global meta tags and font imports
    - Added global <head> configuration under the `app` property in nuxt.config.ts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-    css: [
-    './app/styles/main.scss'
-  ],
-})
+  css: ["./app/styles/main.scss"],
+  app: {
+    head: {
+      title: "Cooking Blog",
+      meta: [
+        {
+          name: "description",
+          content: "Discover delicious recipes and culinary secrets.",
+        },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Inter:wght@300;400;500;600&display=swap",
+        },
+      ],
+    },
+  },
+});
