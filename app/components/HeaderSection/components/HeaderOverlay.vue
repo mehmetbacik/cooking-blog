5fa59574f8c54ba9c19f9f756aaf1162367931a9
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-03 22:27:47 +0300

    feat: split Header into dynamic components, add HeaderOverlay.vue
    - Split HeaderSection into dynamic components for better manageability.
    - Added HeaderOverlay.vue
    - Supports future updates and responsive behavior for the header.

---
+++ app/components/HeaderSection/components/HeaderOverlay.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
+
+<template>
+  <div class="header__overlay" @click="$emit('click')"></div>
+</template>
