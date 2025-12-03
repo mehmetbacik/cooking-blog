8109cd665c6a864c94d1f8cd6712da90ffc94a66
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-03 22:26:24 +0300

    feat: split Header into dynamic components, add SocialLinks.vue
    - Split HeaderSection into dynamic components for better manageability.
    - Added SocialLinks.vue
    - Supports future updates and responsive behavior for the header.

---
+++ app/components/HeaderSection/components/SocialLinks.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  socialItems: Array<{ id: string | number; name: string; link: string; iconUrl: string }>;
+  mobile?: boolean;
+}>();
+</script>
+
+<template>
+  <div :class="['social__links', mobile ? 'header__social-wrapper--mobile' : '']">
+    <a
+      v-for="social in props.socialItems"
+      :key="social.id"
+      :href="social.link"
+      target="_blank"
+      :aria-label="social.name"
+      class="social-item"
+    >
+      <img :src="social.iconUrl" :alt="social.name" />
+    </a>
+  </div>
+</template>
