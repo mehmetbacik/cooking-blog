4b261a683338dd8efdcab7f1a95f806457a9de1d
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-03 22:22:46 +0300

    feat: split Header into dynamic components, add HeaderLogo.vue
    - Refactored HeaderSection component into smaller, dynamic components.
    - Added HeaderLogo.vue as the sub-component.
    - Prepared structure for easier management of header elements and future extensions.

---
+++ app/components/HeaderSection/components/HeaderLogo.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+import { Icon } from "@iconify/vue";
+
+const props = defineProps<{
+  isMenuOpen: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "toggleMenu"): void;
+  (e: "closeMenu"): void;
+}>();
+
+const LogoPath = "/images/logo.svg";
+
+const toggleMenu = () => emit("toggleMenu");
+const closeMenu = () => emit("closeMenu");
+</script>
+
+<template>
+  <div class="col-12 col-lg-3 header__logo-wrapper">
+    <NuxtLink to="/" class="header__logo" @click="closeMenu">
+      <img :src="LogoPath" alt="Logo" />
+    </NuxtLink>
+    <button
+      class="header__menu-toggle"
+      @click="toggleMenu"
+      aria-label="Open menu"
+      :aria-expanded="props.isMenuOpen"
+    >
+      <Icon icon="mdi:menu" class="header__menu-icon" />
+    </button>
+  </div>
+</template>
