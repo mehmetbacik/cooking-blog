48eb854a13a4989127cfdaa4c64d174a0d87185c
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-14 23:22:35 +0300

    feat: add HeroSlideFooter with author and CTA
    - Added HeroSlideFooter component to display author information and the primary call-to-action button.

---
+++ app/components/HeroSection/components/HeroSlideFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { SlideItem } from "../../../types";
+defineProps<{ slide: SlideItem }>();
+</script>
+
+<template>
+  <footer class="slide__footer">
+    <div class="author-wrapper">
+      <span class="author-photo">
+        <img :src="slide.photo" :alt="slide.author" />
+      </span>
+
+      <div class="author-content">
+        <span class="author">{{ slide.author }}</span>
+        <span class="date">{{ slide.date }}</span>
+      </div>
+    </div>
+
+    <NuxtLink :to="slide.link" class="hero-button">
+      {{ slide.text }}
+    </NuxtLink>
+  </footer>
+</template>
