d4e1857861f2f6c44dac73177e5c58f923773a55
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-15 23:15:45 +0300

    feat: implement CategoriesSection structure with data integration
    - Built initial layout and rendering logic for category items.
    - Integrated category data from data file.

--- app/components/CategoriesSection/index.vue
+++ app/components/CategoriesSection/index.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { categoriesData } from "../../data/categoriesData";
+</script>
 
 <template>
   <section class="categories">
-    <div class="container categories__content">Categories</div>
+    <div class="container categories__content">
+      <div class="categories__header">
+        <h2>Categories</h2>
+        <a href="/recipes" class="categories__all-link"> View All Categories </a>
+      </div>
+      <div class="categories__list">
+        <div
+          v-for="category in categoriesData"
+          :key="category.id"
+          class="categories__item"
+        >
+          <a :href="category.url" class="categories__link">
+            <div class="categories__image-wrapper">
+              <img
+                :src="category.image"
+                :alt="category.title"
+                class="categories__image"
+              />
+            </div>
+
+            <span class="categories__title">
+              {{ category.title }}
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
