606f793c0a97bdb365ec74249395c6dcd1d42d8e
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-17 23:38:41 +0300

    feat: create CategoriesCard component for individual category cards
    - CategoriesCard component, representing a single category card, has been created.

---
+++ app/components/CategoriesSection/components/CategoriesCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { CategoryItem } from "../../../types";
+
+defineProps<{
+  category: CategoryItem;
+}>();
+</script>
+
+<template>
+  <div class="categories__item">
+    <a :href="category.url" class="categories__link">
+      <div class="categories__image-wrapper">
+        <img
+          :src="category.image"
+          :alt="category.title"
+          class="categories__image"
+        />
+      </div>
+      <span class="categories__item-title">
+        {{ category.title }}
+      </span>
+    </a>
+  </div>
+</template>
