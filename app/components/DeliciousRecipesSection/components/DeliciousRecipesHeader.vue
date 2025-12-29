067553dac0c1f6af2dc87c6f86361b614c32cabe
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-12-29 14:04:59 +0300

    feat: add DeliciousRecipesHeader component with styles
    - Implemented DeliciousRecipesHeader.vue component
    - Added and configured style definitions for the header
    - Structured component layout for recipe section header

---
+++ app/components/DeliciousRecipesSection/components/DeliciousRecipesHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="deliciousRecipes__header">
+    <h2 class="deliciousRecipes__title">
+      Try this delicious recipe to make your day
+    </h2>
+    <p class="deliciousRecipes__desc">
+      Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqut enim ad minim
+    </p>
+  </div>
+</template>
--- app/components/DeliciousRecipesSection/index.vue
+++ app/components/DeliciousRecipesSection/index.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import DeliciousRecipesHeader from "./components/DeliciousRecipesHeader.vue";
+</script>
 
 <template>
   <section class="deliciousRecipes">
-    <div class="container deliciousRecipes__content">Delicious Recipes</div>
+    <div class="container deliciousRecipes__content">
+      <DeliciousRecipesHeader />
+    </div>
   </section>
 </template>
