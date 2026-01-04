69945a512c0a1bb13139553ca92b70aedca878fe
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-04 23:42:18 +0300

    feat: add footer logo and brand description
    - Added brand logo with homepage link
    - Included short brand description text
    - Isolated branding logic into a reusable footer component

---
+++ app/components/FooterSection/components/FooterLogo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const logoPath = "/images/logo.svg";
+</script>
+
+<template>
+  <div class="footer__logo-wrapper">
+    <NuxtLink to="/" class="footer__logo">
+      <img :src="logoPath" alt="Logo" />
+    </NuxtLink>
+
+    <p class="footer__logo-desc">
+      Lorem ipsum dolor sit amet, consectetuipisicing elit
+    </p>
+  </div>
+</template>
