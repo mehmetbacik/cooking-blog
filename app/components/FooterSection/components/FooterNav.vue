9f51a2349207882b951a0c0342a498c889838d45
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-04 23:42:55 +0300

    feat: implement footer navigation links
    - Rendered footer navigation using centralized nav data
    - Ensured scalable structure for future navigation updates
    - Kept navigation logic isolated from layout components

---
+++ app/components/FooterSection/components/FooterNav.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { navItems } from "../../../data/footerNavData";
+</script>
+
+<template>
+  <div class="footer__nav-wrapper">
+    <nav class="footer__nav">
+      <NuxtLink
+        v-for="item in navItems"
+        :key="item.id"
+        :to="item.link"
+        class="footer__nav-item"
+      >
+        {{ item.label }}
+      </NuxtLink>
+    </nav>
+  </div>
+</template>
