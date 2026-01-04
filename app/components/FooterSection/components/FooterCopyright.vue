f93d5733b381034e32a0787d39db5472717e9245
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-04 23:44:15 +0300

    feat: add footer copyright
    - Added copyright and platform attribution
    - Centralized footer meta information for better structure

---
+++ app/components/FooterSection/components/FooterCopyright.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="footer__copyright">
+    <span>
+      © 2020 Flowbase. Powered by
+      <NuxtLink to="#" class="footer__copyright-company"> Webflow </NuxtLink>
+    </span>
+  </div>
+</template>
