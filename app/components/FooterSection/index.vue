c9a99e3f60c90063d383e50fde4423443918d2e1
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-04 23:45:24 +0300

    feat: compose footer layout using modular subcomponents
    - Created main Footer component as a layout wrapper
    - Integrated newsletter, navigation, logo, social, and copyright sections
    - Improved readability and maintainability by delegating logic to child components

--- app/components/FooterSection/index.vue
+++ app/components/FooterSection/index.vue
@@ -1,79 +1,23 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { navItems } from "../../data/footerNavData";
-import { socialItems } from "../../data/socialData";
-
-const LogoPath = "/images/logo.svg";
-
-const email = ref("");
-
-const handleSubscribe = () => {
-  if (!email.value) return;
-  console.log("Subscribed:", email.value);
-  alert("Thank you! You have subscribed to our newsletter.");
-  email.value = "";
-};
+import FooterNewsletter from "./components/FooterNewsletter.vue";
+import FooterNav from "./components/FooterNav.vue";
+import FooterLogo from "./components/FooterLogo.vue";
+import FooterCopyright from "./components/FooterCopyright.vue";
+import FooterSocial from "./components/FooterSocial.vue";
 </script>
 
 <template>
   <footer class="footer">
     <div class="container footer__content">
-      <div class="footer__newsletter">
-        <h4 class="footer__newsletter-title">Deliciousness to your inbox</h4>
-        <p class="footer__newsletter-desc">
-          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
-          tempor <br />incididunt ut labore et dolore magna aliqut enim ad minim
-        </p>
-        <form @submit.prevent="handleSubscribe" class="footer__newsletter-form">
-          <input
-            v-model="email"
-            type="email"
-            placeholder="Your email address..."
-            required
-          />
-          <button type="submit">Subscribe</button>
-        </form>
-      </div>
+      <FooterNewsletter />
       <div class="footer__bottom">
         <div class="footer__nav-group">
-          <div class="footer__logo-wrapper">
-            <NuxtLink to="/" class="footer__logo">
-              <img :src="LogoPath" alt="Logo" />
-            </NuxtLink>
-            <p class="footer__logo-desc">
-              Lorem ipsum dolor sit amet, consectetuipisicing elit
-            </p>
-          </div>
-          <div class="footer__nav-wrapper">
-            <nav class="footer__nav">
-              <NuxtLink
-                v-for="item in navItems"
-                :key="item.id"
-                :to="item.link"
-                class="footer__nav-item"
-              >
-                {{ item.label }}
-              </NuxtLink>
-            </nav>
-          </div>
+          <FooterLogo />
+          <FooterNav />
         </div>
         <div class="footer__meta">
-          <div class="footer__copyright">
-            <span>&copy; 2020 Flowbase. Powered by <NuxtLink to="#" class="footer__copyright-company">Webflow</NuxtLink></span>
-            
-          </div>
-          <div class="footer__social">
-            <NuxtLink
-              v-for="social in socialItems"
-              :key="social.id"
-              :to="social.link"
-              target="_blank"
-              class="footer__social-item"
-              :aria-label="social.name"
-            >
-              <img :src="social.iconUrl" :alt="social.name" />
-            </NuxtLink>
-          </div>
+          <FooterCopyright />
+          <FooterSocial />
         </div>
       </div>
     </div>
