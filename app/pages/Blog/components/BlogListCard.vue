1b3d7225654e576d1bcd37f6921536c932b2484d
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-09 14:51:58 +0300

    feat: add BlogListCard component
    - Created BlogListCard.vue component under Blog page components
    - Structured card layout for blog list items
    - Prepared component for blog data rendering

---
+++ app/pages/Blog/components/BlogListCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{
+  post: {
+    id: number;
+    title: string;
+    excerpt: string;
+    image: string;
+    slug: string;
+    author: string;
+    photo: string;
+    date: string;
+  };
+}>();
+</script>
+
+<template>
+  <article class="blog-entry">
+    <div class="entry-image">
+      <img :src="post.image" :alt="post.title" loading="lazy" />
+    </div>
+
+    <div class="entry-content">
+      <NuxtLink :to="`/blog/${post.slug}`">
+        <h2>{{ post.title }}</h2>
+      </NuxtLink>
+
+      <p>{{ post.excerpt }}</p>
+
+      <div class="entry-meta">
+        <span>{{ post.date }}</span>
+      </div>
+
+      <div class="author">
+        <img :src="post.photo" :alt="post.author" />
+        <span>{{ post.author }}</span>
+      </div>
+    </div>
+  </article>
+</template>
