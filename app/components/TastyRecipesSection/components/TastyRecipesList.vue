2fb47a79ccfa7e69b841015a8e041aadbace1823
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-12 20:11:44 +0300

    feat: add TastyRecipesList component
    - Created TastyRecipesList.vue component
    - Implemented list structure for rendering recipe cards
    - Prepared component for integration with TastyRecipesCard

---
+++ app/components/TastyRecipesSection/components/TastyRecipesList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { TastyRecipeItem } from "../../../types";
+import TastyRecipesCard from "./TastyRecipesCard.vue";
+defineProps<{
+  tastyRecipes: TastyRecipeItem[];
+}>();
+</script>
+
+<template >
+  <div class="tastyRecipes__list">
+    <template
+      v-for="(tastyRecipe, index) in tastyRecipes"
+      :key="tastyRecipe.id"
+    >
+      <TastyRecipesCard :tastyRecipe="tastyRecipe" />
+    </template>
+  </div>
+</template>
+s
