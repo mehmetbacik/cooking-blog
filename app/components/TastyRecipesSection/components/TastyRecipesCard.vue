887027ad3f5875b2c630eaaa46668b1cae1c9e6a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-12 20:09:53 +0300

    feat: add TastyRecipesCard component
    - Created TastyRecipesCard.vue as the first component of TastyRecipesSection
    - Structured card layout for tasty recipe items
    - Prepared component for integration with tastyRecipes data

---
+++ app/components/TastyRecipesSection/components/TastyRecipesCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { TastyRecipeItem } from "../../../types";
+import { useTextTruncation } from "../../../helpers/useTextTruncation";
+
+const props = defineProps<{
+  tastyRecipe: TastyRecipeItem;
+}>();
+
+const { truncatedTitle } = useTextTruncation(props.tastyRecipe.title);
+</script>
+
+<template>
+  <div class="tastyRecipes__item">
+    <NuxtLink :to="`/recipes/${tastyRecipe.slug}`" class="tastyRecipes__link">
+      <div class="tastyRecipes__image-wrapper">
+        <img
+          :src="tastyRecipe.image"
+          :alt="tastyRecipe.title"
+          class="tastyRecipes__image"
+          loading="lazy"
+        />
+      </div>
+
+      <span class="tastRecipes__item-title">
+        {{ truncatedTitle }}
+      </span>
+      <div class="tastyRecipes__item-meta">
+        <span class="tastyRecipes__item-author">
+          {{ tastyRecipe.author }}
+        </span>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
