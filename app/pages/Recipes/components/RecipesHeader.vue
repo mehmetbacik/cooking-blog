9b2f551cf5159f6c3c3858dfa5794b14102eac72
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-15 22:00:45 +0300

    feat: add RecipesHeader component
    - Created RecipesHeader.vue component for Recipes page structure
    - Implemented header layout for recipes listing page
    - Prepared component for integration with Recipes page

---
+++ app/pages/Recipes/components/RecipesHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="recipesList__header">
+    <h2 class="recipesList__title">Recipes</h2>
+    <p class="recipesList__desc">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore
+    </p>
+  </div>
+</template>
