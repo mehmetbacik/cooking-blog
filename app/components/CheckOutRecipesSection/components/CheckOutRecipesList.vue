98cba817673e1ab642dcc4b09478f561fac2d5a5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-18 17:00:42 +0300

    feat: add CheckOutRecipesList component
    - Created CheckOutRecipesList.vue component
    - Implemented list structure for checkout recipe cards
    - Prepared component for integration with CheckOutRecipesCard and data

---
+++ app/components/CheckOutRecipesSection/components/CheckOutRecipesList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { CheckOutRecipeItem } from "../../../types";
+import CheckOutRecipesCard from "./CheckOutRecipesCard.vue";
+defineProps<{
+  checkOutRecipes: CheckOutRecipeItem[];
+}>();
+</script>
+
+<template >
+  <div class="checkOutRecipes__list">
+    <template
+      v-for="(checkOutRecipe, index) in checkOutRecipes"
+      :key="checkOutRecipe.id"
+    >
+      <CheckOutRecipesCard :checkOutRecipe="checkOutRecipe" />
+    </template>
+  </div>
+</template>
