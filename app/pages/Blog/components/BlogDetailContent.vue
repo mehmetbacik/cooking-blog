6a85fe2c4f17d4a1e387c03b44b31806acc8a8eb
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-24 12:36:58 +0300

    feat: add BlogDetailContent component for post body rendering
    - Created BlogDetailContent component for rendering blog post body
    - Structured content rendering for improved readability
    - Prepared component for flexible content layouts
    - Integrated content component into blog detail page

---
+++ app/pages/Blog/components/BlogDetailContent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { BlogPost } from "../../../types";
+
+defineProps<Pick<BlogPost, "content">>();
+</script>
+
+<template>
+  <div class="blogDetail__mainLayout" v-html="content"></div>
+</template>
