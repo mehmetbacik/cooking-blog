417ca86a69fb5fabe0264a9e5ebc0e6d4d07e180
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-24 12:34:13 +0300

    feat: add BlogDetailHeader and BlogDetailMeta components
    - Created BlogDetailHeader component for blog detail page
    - Created BlogDetailMeta component for author, date, and meta information
    - Improved blog detail page structure and readability
    - Prepared components for reusable blog detail layouts

---
+++ app/pages/Blog/components/BlogDetailHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { BlogPost } from "../../../types";
+
+import BlogDetailMeta from "./BlogDetailMeta.vue";
+
+defineProps<{
+  post: BlogPost;
+}>();
+</script>
+
+<template>
+  <div class="blogDetail__headerLayout">
+    <div class="blogDetail__header">
+      <h1 class="blogDetail__title">{{ post.title }}</h1>
+
+      <BlogDetailMeta
+        :author="post.author"
+        :date="post.date"
+        :photo="post.photo"
+      />
+
+      <div class="blogDetail__summary">
+        <p>{{ post.summary }}</p>
+      </div>
+
+      <div class="blogDetail__image">
+        <img :src="post.image" :alt="post.title" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/pages/Blog/components/BlogDetailMeta.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { BlogPost } from "../../../types";
+
+
+defineProps<Pick<BlogPost, "author" | "date" | "photo">>();
+</script>
+
+<template>
+  <div class="blogDetail__meta">
+    <div class="blogDetail__author">
+      <img :src="photo" :alt="author" />
+      <span>{{ author }}</span>
+    </div>
+    <div class="blogDetail__date">
+      <span>{{ date }}</span>
+    </div>
+  </div>
+</template>
