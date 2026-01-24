080170b3c34db15df0f9b0d036ae5c0ff04ac89c
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-01-24 12:35:23 +0300

    feat: add BlogDetailShare component with social share actions
    - Created BlogDetailShare component for blog detail pages
    - Implemented social share actions for supported platforms
    - Integrated share component into blog detail layout
    - Improved content sharing capability and user engagement

---
+++ app/pages/Blog/components/BlogDetailShare.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useBlogShare } from "@/composables/useBlogShare";
+
+const fbIcon = "/icons/facebook.svg";
+const twIcon = "/icons/twitter.svg";
+const inIcon = "/icons/instagram.svg";
+
+const {
+  shareFacebook,
+  shareTwitter,
+  shareInstagram,
+} = useBlogShare();
+</script>
+
+<template>
+  <div class="blogDetail__shareLayout">
+    <span>Share This On:</span>
+
+    <div class="blogDetail__shareIcon">
+      <button
+        class="blog-share__btn blog-share__btn--facebook"
+        @click="shareFacebook"
+        aria-label="Share on Facebook"
+      >
+        <img :src="fbIcon" alt="Facebook Icon" />
+      </button>
+
+      <button
+        class="blog-share__btn blog-share__btn--twitter"
+        @click="shareTwitter"
+        aria-label="Share on Twitter"
+      >
+        <img :src="twIcon" alt="Twitter Icon" />
+      </button>
+
+      <button
+        class="blog-share__btn blog-share__btn--instagram"
+        @click="shareInstagram"
+        aria-label="Share on Instagram"
+      >
+        <img :src="inIcon" alt="Instagram Icon" />
+      </button>
+    </div>
+  </div>
+</template>
