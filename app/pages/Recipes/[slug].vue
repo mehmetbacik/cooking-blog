3473e921bcbbc92f7665960e75d6e5aca26c6ea5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2026-02-07 00:11:22 +0300

    style: update RecipeDetail styling
    - Refined SCSS structure and improved visual hierarchy
    - Updated spacing, typography and layout adjustments
    - Enhanced responsive behavior for RecipeDetail component

--- app/pages/Recipes/[slug].vue
+++ app/pages/Recipes/[slug].vue
@@ -356,36 +356,43 @@ const isDescriptionLong = computed(() => {
                 >Ingredients for this recipe are being prepared.</span
               >
             </div>
-            <div v-if="recipe.steps?.length" class="steps-wrapper">
-              <h3 class="section-heading">Instructions</h3>
-              <div class="steps-list">
+            <div v-if="recipe.steps?.length" class="recipeDetail__stepsWrapper">
+              <h3 class="recipeDetail__stepsTitle">Directions</h3>
+              <div class="recipeDetail__stepsList">
                 <div
                   v-for="(step, idx) in recipe.steps"
                   :key="idx"
-                  class="step-item"
-                  :class="{ 'is-completed': checkedSteps.has(idx) }"
+                  class="recipeDetail__stepsItem"
+                  :class="{ 'is-checked': checkedSteps.has(idx) }"
                   @click="toggleStep(idx)"
                 >
-                  <div class="step-marker">
-                    <span v-if="!checkedSteps.has(idx)">{{ idx + 1 }}</span>
-                    <svg v-else>...</svg>
+                  <div class="recipeDetail__stepsCheckbox">
+                    <svg
+                      v-if="checkedSteps.has(idx)"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="check-icon"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
                   </div>
-                  <div class="step-content">
-                    <h4 v-if="step.title" class="step-title">
+                  <div class="recipeDetail__stepsContent">
+                    <h4 v-if="step.title" class="recipeDetail__stepsContent-title">
                       {{ step.title }}
                     </h4>
-                    <p v-if="step.text" class="step-text">
-                      {{ step.text }}
-                    </p>
+                    <p v-if="step.text" class="recipeDetail__stepsContent-text">{{ step.text }}</p>
                     <img
                       v-if="step.image"
                       :src="step.image"
                       :alt="step.title || step.text"
-                      class="step-image"
+                      class="recipeDetail__stepsContent-image"
                     />
-                    <p v-if="step.text2" class="step-text">
-                      {{ step.text2 }}
-                    </p>
+                    <p v-if="step.text2" class="recipeDetail__stepsContent-text">{{ step.text2 }}</p>
                   </div>
                 </div>
               </div>
